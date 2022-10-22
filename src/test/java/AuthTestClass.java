f4f2b1760f674aebd1abc81e5e9616e32069846d
MarwaHashem <marmarhashem26@gmail.com>
2022-10-22 18:10:28 +0200

    Adding files

---
+++ src/test/java/AuthTestClass.java
@@ -0,0 +1,114 @@
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class AuthTestClass {
+String loginToken=null;
+String bookingId=null;
+   @BeforeClass
+    public void login (){
+      String endPointUrl="https://restful-booker.herokuapp.com/auth";
+       String body= """
+        {
+            "username" : "admin",
+            "password" : "password123"
+        }
+         """;
+        ValidatableResponse validatableResponse=given().body(body)
+                .header("Content-Type","application/json")
+                .when().post(endPointUrl).then();
+
+        Response response= validatableResponse.extract().response();
+        JsonPath jsonPath=response.jsonPath();
+        loginToken=jsonPath.getString("token");
+        bookingId=jsonPath.getString("booking.bookingid");
+    }
+    @Test(priority = 0)
+    public void testCreateBooking(){
+     String endPointUrl="https://restful-booker.herokuapp.com/booking";
+     String body= """
+             {
+                 "firstname" : "Jim",
+                 "lastname" : "Brown",
+                 "totalprice" : 111,
+                 "depositpaid" : true,
+                 "bookingdates" : {
+                     "checkin" : "2018-01-01",
+                     "checkout" : "2019-01-01"
+                 },
+                 "additionalneeds" : "Breakfast"
+             }
+              """;
+          var   validatableResponse= given().body(body)
+                  .header("Content-Type","application/json")
+
+                  .when().post(endPointUrl).then();
+          validatableResponse.body("booking.firstname",equalTo("Jim"));
+           validatableResponse.body("booking.lastname",equalTo("Brown"));
+          validatableResponse.body("booking.totalprice",equalTo( 111 ));
+
+        Response response= validatableResponse.extract().response();
+        JsonPath jsonPath=response.jsonPath();
+        bookingId=jsonPath.getString("bookingid");
+    }
+@Test(priority = 1, dependsOnMethods = "testCreateBooking")
+    public void testEditBooking(){
+
+        String endPointUrl="https://restful-booker.herokuapp.com/booking/" +bookingId;
+        String body= """
+                {
+                    "firstname" : "James",
+                    "lastname" : "Brown",
+                    "totalprice" : 111,
+                    "depositpaid" : true,
+                    "bookingdates" : {
+                        "checkin" : "2018-01-01",
+                        "checkout" : "2019-01-01"
+                    },
+                    "additionalneeds" : "Breakfast"
+                }
+                 """;
+        var   validatableResponse= given().body(body)
+                .header("Content-Type","application/json")
+                .header("Cookie","token=" + loginToken)
+                .header("Authorisation","Basic")
+                .when().put(endPointUrl).then();
+
+    validatableResponse.body("firstname",equalTo("James"));
+    validatableResponse.body("lastname",equalTo("Brown"));
+    validatableResponse.body("totalprice",equalTo( 111 ));
+    }
+
+    @Test(priority = 2, dependsOnMethods = "testEditBooking")
+    public void testGetBooking(){
+        String endPointUrl="https://restful-booker.herokuapp.com/booking/" +bookingId;
+        var validatableResponse = given()
+                .header("Accept","application/json")
+                .when().get(endPointUrl).then();
+        validatableResponse.body("firstname",equalTo("James"));
+        validatableResponse.body("lastname",equalTo("Brown"));
+        validatableResponse.body("totalprice",equalTo( 111 ));
+    }
+
+   @Test(priority = 3, dependsOnMethods = "testGetBooking")
+    public void testDeleteBooking(){
+        String endPointUrl="https://restful-booker.herokuapp.com/booking/" +bookingId;
+
+        var validatableResponse = given()
+                .header("Content-Type" , "application/json")
+                .header("Cookie","token="+ loginToken)
+                .header("Authorisation","Basic")
+                .when().delete(endPointUrl).then();
+
+        Response response= validatableResponse.extract().response();
+        Assert.assertEquals(response.asString(),"Created");
+
+    }
+
+
+}
