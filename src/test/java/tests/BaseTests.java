b9daa281ff49641c15d63a9a7ac7af93c7debf9b
MarwaHashem <marmarhashem26@gmail.com>
2022-10-25 17:12:47 +0200

    Modified files

---
+++ src/test/java/tests/BaseTests.java
@@ -0,0 +1,27 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import utils.logs.Log;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+
+import utils.logs.Log;
+
+public class BaseTests {
+
+    public WebDriver driver;
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+
+
+
+
+
+}
